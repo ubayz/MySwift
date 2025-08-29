0d804dab9428c4057fb62930c8b007338c408d9a
Bayu Rachmawan Rahmat <bayu.rachmawan@bankbsi.co.id>
2025-08-29 13:22:48 +0700

    penambahan childview dengan logic dan manipulasi data sederhana

--- MySwift/Feature/EmbededView/ChildView2/ChildView2.swift
+++ MySwift/Feature/EmbededView/ChildView2/ChildView2.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
+protocol ChildView2Delegate: AnyObject {
+    func didTapNavigate()
+}
+
 class ChildView2: UIView {
     
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var input1: UITextField!
+    @IBOutlet weak var input2: UITextField!
     
-    static let identifier = String(describing: ChildView2.self)
-    
-    static func nib() -> UINib {
-        return UINib(nibName: identifier, bundle: nil)
+    weak var delegate: ChildView2Delegate?
+    var hasil: Int = 0
+
+    func logicProcess() {
+        let int1 = Int(input1.text ?? "") ?? 0
+        let int2 = Int(input2.text ?? "") ?? 0
+        hasil = int1 + int2
+        label.text = String(describing: hasil)
     }
+    
+    @IBAction func proses(_ sender: Any) {
+        logicProcess()
+    }
+    
+    @IBAction func navigasi(_ sender: Any) {
+        delegate?.didTapNavigate()
+    }
+    
 }
--- MySwift/Feature/EmbededView/EmbededView.swift
+++ MySwift/Feature/EmbededView/EmbededView.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class EmbededView: BaseViewController {
+class EmbededView: BaseViewController, ChildView2Delegate {
     
     @IBOutlet weak var ChildViewContainer: UIView!
     
@@ -16,10 +16,25 @@ class EmbededView: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Embeded View"
-        
-        if let childView: ChildView1 = ChildView1.loadFromNib() {
-            childView.label.text = "BAYU"
-            childView.embedView(in: ChildViewContainer)
+
+    }
+    
+    @IBAction func childView1(_ sender: Any) {
+        if let childView1: ChildView1 = ChildView1.loadFromNib() {
+            childView1.label.text = "Child View 1"
+            childView1.embedView(in: ChildViewContainer)
         }
     }
+    
+    @IBAction func childView2(_ sender: Any) {
+        if let childView2: ChildView2 = ChildView2.loadFromNib() {
+            childView2.delegate = self
+            childView2.embedView(in: ChildViewContainer)
+        }
+    }
+    
+    func didTapNavigate(){
+        self.navigationController?.popViewController(animated: true)
+    }
+    
 }
